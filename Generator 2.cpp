a41e311e56da58d9816423600a8ac20c91b1233f
Michael-Henson <michael.a.henson@okstate.edu>
2023-12-07 12:57:00 -0600

    resize

--- Generator 2.cpp
+++ Generator 2.cpp
@@ -5,10 +5,11 @@ using namespace std;
 
 int main(){
 	int yes;
-	int i = 0, j = 0, k = 255;
-	for(j = 0; j < 16; ++j){
+	int size = 24;
+	int i = 0, j = 0, k = (size * size - 1);
+	for(j = 0; j < size; ++j){
 		printf("//Row %i\n", j + 1);
-		for(i = 0; i < 16; ++i){
+		for(i = 0; i < size; ++i){
 			printf("if((cy >= START+SEGMENT*%i) && (cy <= START+SEGMENT*%i-SKIP) && (cx >= START+SEGMENT*%i) && (cx <= START+SEGMENT*%i-SKIP))\n", j, j + 1, i, i + 1);
 			printf("\tif (n2[%i] == 1'b0)\n", k);
 			printf("\t\tDataIn <= dead;\n");
@@ -23,12 +24,12 @@ int main(){
 		printf("// Begining/End of Row\n");
 		printf("if ((cy >= START+SEGMENT*%i) && (cy <= START+SEGMENT*%i-SKIP) && (cx < START))\n", j, j + 1);
 		printf("\tDataIn <= {8'h00, 8'h00, 8'h00};\n");
-		printf("if ((cy >= START+SEGMENT*%i) && (cy <= START+SEGMENT*%i-SKIP) && (cx > START+SEGMENT*16))\n", j, j + 1);
+		printf("if ((cy >= START+SEGMENT*%i) && (cy <= START+SEGMENT*%i-SKIP) && (cx > START+SEGMENT*%i))\n", j, j + 1, size);
 		printf("\tDataIn <= {8'h00, 8'h00, 8'h00};\n");
 		printf("\n");
 		
 		printf("// Skip Row\n");
-		for(i = 0; i < 16; ++i){
+		for(i = 0; i < size; ++i){
 			printf("if((cy >= START+SEGMENT*%i) && (cy <= START+SEGMENT*%i-SKIP) && (cx >= START+SEGMENT*%i-SKIP) && (cx <= START+SEGMENT*%i))\n", j, j + 1, i + 1, i + 1);
 			printf("\tDataIn <= {8'h00, 8'h00, 8'h00};\n");
 		}
